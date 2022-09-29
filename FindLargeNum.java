e14793ae0426656a1b1091fa2224d4f0efbcdbb1
VishuSa001 <114149406+VishuSa001@users.noreply.github.com>
2022-09-29 05:11:22 -0700

    Add files via upload

---
+++ FindLargeNum.java
@@ -0,0 +1,20 @@
+
+//  Create a program to find largest of two numbers. */
+import java.util.*;
+
+public class FindLargeNum {
+    public static void main(String[] args) {
+        // take a input from the user types and variable
+        Scanner sc = new Scanner(system.in);
+        System.out.println(" enter the two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // check the condition and print
+        if (a > b) {
+            System.out.println("is this a largest number");
+        } else {
+            System.out.println("lowest");
+        }
+
+    }
+}
