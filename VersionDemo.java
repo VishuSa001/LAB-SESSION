d00c344e838d992983b14df983addb1e4061c4e8
VishuSa001 <114149406+VishuSa001@users.noreply.github.com>
2022-09-29 05:28:15 -0700

    Add files via upload

---
+++ VersionDemo.java
@@ -0,0 +1,12 @@
+/* program::find the current version of java
+  @author: Vishal Saini
+@date: 29 september 2022
+*/
+
+class VersionDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Vesion of Java is " + System.getProperty("java.version"));
+    }
+
+}
