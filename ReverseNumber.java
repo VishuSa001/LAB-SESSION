a96d446764d1090a31f39cf3c6350f4beb2f1c95
VishuSa001 <114149406+VishuSa001@users.noreply.github.com>
2022-10-16 10:40:24 -0700

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,5 +1,8 @@
+// Reverse Number using Loops and Array
+
 public class ReverseNumber {
     public static void main(String[] args) {
+        // initialize ,declare ,print
         int[] marks = { 56, 65, 43, 87, 54 };
         for (int i = marks.length - 1; i >= 0; i--) {
             System.out.println(marks[i]);
