4fd6b81b21b53955df002eb5ef1e363f93bbbfc0
VishuSa001 <114149406+VishuSa001@users.noreply.github.com>
2022-10-27 21:03:49 -0700

    Add files via upload

---
+++ HappyDiwali.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HappyDiwali {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter a Stringline");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("happy Diwali");
+        }
+
+    }
+}
